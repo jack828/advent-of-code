3ba012ed9dfb0cb8081813538c82e62958225d02
Jack Burgess <jackburgess124@gmail.com>
2022-12-20 21:10:35 +0000

    aoc 18 p2

--- 2022/18/main.c
+++ 2022/18/main.c
@@ -8,11 +8,13 @@
 #include "../../utils.h"
 
 #define MAX_CUBES 2030
-#define MAX_X 100
-#define MAX_Y 100
-#define MAX_Z 100
+#define MAX_X 30
+#define MAX_Y 30
+#define MAX_Z 30
 char grid[MAX_X][MAX_Y][MAX_Z] = {0};
 char visited[MAX_X][MAX_Y][MAX_Z] = {0};
+
+enum { EMPTY = 0, ROCK = 1, WATER = 2 };
 
 typedef struct cube_t {
   int x;
@@ -31,19 +33,41 @@ void lineHandler(char *line) {
   int y = atoi(strtok(NULL, ","));
   int z = atoi(strtok(NULL, ","));
 
-  grid[x][y][z] = 1;
+  grid[x][y][z] = ROCK;
   cube_t *cube = malloc(sizeof(cube_t));
   cube->x = x;
   cube->y = y;
   cube->z = z;
   cubes[cubeIndex++] = cube;
-
-  fprintf(stdout, "cubes[%d] (%d,%d,%d)\n", cubeIndex - 1, cube->x, cube->y,
-          cube->z);
-  fputs("\n", stdout);
 }
 
-void seek(int x, int y, int z) {}
+void flood(int x, int y, int z) {
+  // stop if we hit the edge of the grid
+  if (x < 0 || x == MAX_X) {
+    return;
+  }
+  if (y < 0 || y == MAX_Y) {
+    return;
+  }
+  if (z < 0 || z == MAX_Z) {
+    return;
+  }
+  // stop if we've been here before, or if there is a cube there
+  if (grid[x][y][z] != EMPTY) {
+    return;
+  }
+
+  // mark that we have been here
+  grid[x][y][z] = WATER;
+
+  // spread out
+  flood(x + 1, y, z);
+  flood(x - 1, y, z);
+  flood(x, y + 1, z);
+  flood(x, y - 1, z);
+  flood(x, y, z + 1);
+  flood(x, y, z - 1);
+}
 
 int main() {
   readInput(__FILE__, lineHandler);
@@ -85,10 +109,39 @@ int main() {
   assert(unconnectedSides == 3576);
 #endif
 
-  fprintf(stdout, "Part two: %d\n", 420);
+  flood(0, 0, 0);
+
+  int waterSides = 0;
+
+  for (int i = 0; i < cubeIndex; i++) {
+    cube_t *cube = cubes[i];
+
+    if (grid[cube->x + 1][cube->y][cube->z] == WATER) {
+      waterSides++;
+    }
+    if (cube->x == 0 || grid[cube->x - 1][cube->y][cube->z] == WATER) {
+      waterSides++;
+    }
+
+    if (grid[cube->x][cube->y + 1][cube->z] == WATER) {
+      waterSides++;
+    }
+    if (cube->y == 0 || grid[cube->x][cube->y - 1][cube->z] == WATER) {
+      waterSides++;
+    }
+
+    if (grid[cube->x][cube->y][cube->z + 1] == WATER) {
+      waterSides++;
+    }
+    if (cube->z == 0 || grid[cube->x][cube->y][cube->z - 1] == WATER) {
+      waterSides++;
+    }
+  }
+
+  fprintf(stdout, "Part two: %d\n", waterSides);
 #ifdef TEST_MODE
-  assert(420 == 69);
+  assert(waterSides == 58);
 #else
-  assert(420 == 69);
+  assert(waterSides == 2066);
 #endif
 }

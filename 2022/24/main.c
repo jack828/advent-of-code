8b6cbc2dc896c90898dc64da9e4706b3c2468495
Jack Burgess <jackburgess124@gmail.com>
2022-12-30 22:21:20 +0000

    cleanup

--- 2022/24/main.c
+++ 2022/24/main.c
@@ -164,34 +164,17 @@ void printMap() {
 }
 
 bool isValid(int minute, int y, int x) {
-  // fprintf(stdout, "isvalid %d, (%d, %d) = \n", minute, y, x);
   if (visited[minute][y][x]) {
     return false;
-  }
-  if (y == endY && x == endX) {
-    // fprintf(stdout, "exit found4!\n\n");
-    // fprintf(stdout, "y >= 0\t\t %d\n", y >= 0);
-    // fprintf(stdout, "y <= HEIGHT\t %d\n", y <= HEIGHT);
-    // fprintf(stdout, "x >= 0\t\t %d\n", x >= 0);
-    // fprintf(stdout, "x <= WIDTH\t %d\n", x <= WIDTH);
-    // fprintf(stdout, "map[y][x]\t %c\n", map[y][x]);
-    // return true;
-    // exit(1);
   }
   // is within bounds and not in the wall (there are two gaps for start/end)
   if (y >= 0 && y <= HEIGHT && x >= 0 && x <= WIDTH && map[y][x] == 0) {
     // and does not have a blizzard there (direction/count irrelevant)
-    // fprintf(stdout, "in bound, grid: %c\n", grid[minute][y][x]);
     if (!grid[minute % CYCLES][y][x]) {
-      // && visited[0][y][x] < 4) {
       visited[minute][y][x]++;
-      // fprintf(stdout, "TRUE %c %d\n", grid[minute + 1 % CYCLES][y][x],
       return true;
     }
   }
-  // fprintf(stdout, "false %c %d\n", grid[minute + 1 % CYCLES][y][x],
-  // grid[minute + 1 % CYCLES][y][x]);
-  // fprintf(stdout, "FALSE\n");
   return false;
 }
 
@@ -204,25 +187,13 @@ int main() {
   readInput(__FILE__, lineHandler);
   calculateBlizzards();
 
-  queue_t *queue = q_create();
+  pqueue_t *queue = pq_create();
   point_t *startPoint = malloc(sizeof(point_t));
   startPoint->y = startY;
   startPoint->x = startX;
   startPoint->m = 0;
-  // startPoint->y = 5;
-  // startPoint->x = 6;
-  // startPoint->m = 2;
   startPoint->cost = calculateCost(endY, endX, startPoint->y, startPoint->x);
-  q_enqueue(queue, startPoint);
-
-  point_t *startPoint2 = malloc(sizeof(point_t));
-  startPoint2->y = 20;
-  startPoint2->x = 151;
-  startPoint2->m = 1;
-  // q_enqueue(queue, startPoint2);
-  // q_enqueue(yQueue, 5);
-  // q_enqueue(xQueue, 6);
-  // q_enqueue(mQueue, 2);
+  pq_enqueue(queue, startPoint, startPoint->cost);
 
   fprintf(stdout, "end is at (%d, %d)\n", endY, endX);
 
@@ -230,27 +201,16 @@ int main() {
   int dY[] = {-1, 0, 0, 1, 0};
   int dX[] = {0, -1, 1, 0, 0};
   int time = 0;
-  int loop = 0;
-  while (!q_empty(queue)) {
-    point_t *point = q_dequeue(queue);
-    // fprintf(stdout, "loop: %d - %d, (%d, %d)\n", loop++, point->m, point->y,
-    // point->x);
-    // if (loop > 100) break;
-    // fprintf(stdout, "loop %d, (%d, %d)\n", m, y, x);
-    // dont ask my why there's two
-    // i think it's because i couldn't get the bounds check quite right
+  while (!pq_empty(queue)) {
+    point_t *point = pq_dequeue(queue);
     if (point->y == endY && point->x == endX) {
       fprintf(stdout, "exit found1!\n\n");
       time = point->m;
       break;
     }
-    // if (point->y + 1 == endY && point->x == endX) {
-    //   fprintf(stdout, "exit found2!\n\n");
-    //   time = point->m - 1;
-    //   break;
-    // }
     for (int i = 0; i < 5; i++) {
       point_t *newPoint = malloc(sizeof(point_t));
+      // escape hatch in case of failure
       if (point->m >= 500) {
         continue;
       }
@@ -261,21 +221,16 @@ int main() {
 
       if (isValid(newPoint->m, newPoint->y, newPoint->x)) {
         // fprintf(stdout, "neighbour: %d, (%d, %d)\n", newPoint->m,
-        // newPoint->y, newPoint->x); distance from start int gCost =
-        // calculateCost(startY, startX, newPoint->y, newPoint->x);
-        // int gCost = newPoint->cost;
+        // newPoint->y, newPoint->x); distance from start
+        // int gCost = calculateCost(startY, startX, newPoint->y, newPoint->x);
+        int gCost = newPoint->cost;
         // distance from end
-        // int hCost = calculateCost(endY, endX, newPoint->y, newPoint->x);
-        // int fCost = gCost + hCost;
-        // newPoint->cost = hCost;
-        /* fprintf(stdout, "old m %d, c: %d\n", point->m, point->cost);
-        fprintf(stdout,
-                "m %d, c: %d, gCost: %d, hCost: %d, fCost: "
-                "%d\n",
-                newPoint->m, newPoint->cost, gCost, hCost, fCost); */
+        int hCost = calculateCost(endY, endX, newPoint->y, newPoint->x);
+        int fCost = gCost + hCost;
+        newPoint->cost = fCost;
         // if (newPoint->cost < point->cost) {
-        // fprintf(stdout, "cost ok\n");
-        q_enqueue(queue, newPoint);
+        // pq_enqueue(queue, newPoint);
+        pq_enqueue(queue, newPoint, newPoint->cost);
         // }
       } else {
         free(newPoint);
@@ -283,20 +238,6 @@ int main() {
     }
     free(point);
   }
-
-  // printGrid(2);
-  /*
-  for (int cycle = 0; cycle < CYCLES; cycle++) {
-    fprintf(stdout, "cycle: %d\n", cycle);
-    for (int y = 0; y < HEIGHT; y++) {
-      for (int x = 0; x < WIDTH; x++) {
-        fputc(grid[cycle][y][x] ? grid[cycle][y][x] : '.', stdout);
-      }
-      fputc('\n', stdout);
-    }
-    fputc('\n', stdout);
-  }
-  */
 
   fprintf(stdout, "end is at (%d, %d) - %c %d\n", endY, endX, map[endY][endX],
           map[endY][endX]);

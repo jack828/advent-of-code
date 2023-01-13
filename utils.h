ae8cfc6d6f259747f6ec5c100d50cd25a8bf79b6
Jack Burgess <jackburgess124@gmail.com>
2023-01-13 23:10:21 +0000

    utils

--- utils.h
+++ utils.h
@@ -11,7 +11,8 @@
 #define _AOC_LINE_MAX 1024
 #endif
 
-void readInput_n(const char *file, void (*lineHandler)(char *), int lineLength) {
+void readInput_n(const char *file, void (*lineHandler)(char *),
+                 int lineLength) {
   char path[lineLength];
   path[0] = '\0';
   strcat(path, file);
@@ -29,22 +30,66 @@ void readInput_n(const char *file, void (*lineHandler)(char *), int lineLength) 
   }
   char line[lineLength];
   while (fgets(line, sizeof(line), fp) != NULL) {
+    // trim trailing newline
+    if (strlen(line) > 1) {
+      line[strcspn(line, "\n")] = '\0';
+    }
     lineHandler(line);
   }
   fclose(fp);
 }
 
+void readInput_f(const char *file, void (*lineHandler)(char *),
+                 void (*fileHandler)(int)) {
+  char path[_AOC_LINE_MAX];
+  path[0] = '\0';
+  strcat(path, file);
+  *strrchr(path, '/') = '\0';
+#ifdef TEST_MODE
+  strcat(path, "/example.txt");
+#else
+  strcat(path, "/input.txt");
+#endif
+  fprintf(stdout, "Reading input file: %s\n", path);
+  FILE *fp = fopen(path, "r");
+  if (fp == NULL) {
+    perror("Unable to open file!");
+    exit(1);
+  }
+
+  // count number of lines
+  int c;
+  int newlineCount = 0;
+  while ((c = fgetc(fp)) != EOF) {
+    if (c == '\n')
+      newlineCount++;
+  }
+  fileHandler(newlineCount);
+
+  fseek(fp, 0, 0);
+
+  char line[_AOC_LINE_MAX];
+  while (fgets(line, sizeof(line), fp) != NULL) {
+    // trim trailing newline
+    if (strlen(line) > 1) {
+      line[strcspn(line, "\n")] = '\0';
+    }
+    lineHandler(line);
+  }
+  fclose(fp);
+}
 
 void readInput(const char *file, void (*lineHandler)(char *)) {
   readInput_n(file, lineHandler, _AOC_LINE_MAX);
 }
 
-int max(int a, int b) {
-  return a > b ? a : b;
+void readInputFile(const char *file, void (*lineHandler)(char *),
+                   void (*fileHandler)(int)) {
+  readInput_f(file, lineHandler, fileHandler);
 }
 
-int min(int a, int b) {
-  return a > b ? b : a;
-}
+int max(int a, int b) { return a > b ? a : b; }
+
+int min(int a, int b) { return a > b ? b : a; }
 
 #endif

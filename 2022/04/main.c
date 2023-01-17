4db1a087809b351b2ae49c1a2cdb3f698460552a
Jack Burgess <jackburgess124@gmail.com>
2023-01-17 19:47:56 +0000

    aoc 4 in c

---
+++ 2022/04/main.c
@@ -0,0 +1,56 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+// #define TEST_MODE
+#include "../../utils.h"
+
+int fullyContainedGroups = 0;
+int overlappingGroups = 0;
+
+bool contains(int aLeft, int aRight, int bLeft, int bRight) {
+  return aLeft >= bLeft && aRight <= bRight;
+}
+
+bool overlaps(int aLeft, int aRight, int bLeft, int bRight) {
+  return max(aLeft, aRight) >= min(bLeft, bRight);
+}
+
+void lineHandler(char *line) {
+  fprintf(stdout, "line: %s\n", line);
+  int aLeft;
+  int aRight;
+  int bLeft;
+  int bRight;
+
+  sscanf(line, "%d-%d,%d-%d", &aLeft, &aRight, &bLeft, &bRight);
+  // fprintf(stdout, "%d-%d,%d-%d\n", aLeft, aRight, bLeft, bRight);
+  if (contains(aLeft, aRight, bLeft, bRight) ||
+      contains(bLeft, bRight, aLeft, aRight)) {
+    fullyContainedGroups++;
+  }
+  if (overlaps(aLeft, aRight, bLeft, bRight) &&
+      overlaps(bLeft, bRight, aLeft, aRight)) {
+    overlappingGroups++;
+  }
+}
+
+int main() {
+  readInput(__FILE__, lineHandler);
+
+  fprintf(stdout, "Part one: %d\n", fullyContainedGroups);
+#ifdef TEST_MODE
+  assert(fullyContainedGroups == 2);
+#else
+  assert(fullyContainedGroups == 651);
+#endif
+
+  fprintf(stdout, "Part two: %d\n", overlappingGroups);
+#ifdef TEST_MODE
+  assert(overlappingGroups == 4);
+#else
+  assert(overlappingGroups == 956);
+#endif
+}
